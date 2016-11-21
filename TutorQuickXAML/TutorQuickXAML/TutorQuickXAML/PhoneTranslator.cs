51b4ef4f97ac45dd389d4d5cd35833c0a186ccd6
Stef-R <username@user.noreply.github.com>
2016-11-21 14:49:14 +0100

    PhoneTrans lagt på  nu TTEMP

---
+++ TutorQuickXAML/TutorQuickXAML/TutorQuickXAML/PhoneTranslator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorQuickXAML
+{
+    public static class PhoneTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw))
+                return null;
+            raw = raw.ToUpperInvariant();
+
+            var newNumber = new StringBuilder();
+            foreach (var c in raw)
+            {
+                if ("-0123456789".Contains(c))
+                    newNumber.Append(c);
+                else
+                {
+                    var result = 0;//....
+                }
+            }
+        }
+
+        static int? TranslateToNumber(char c)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (true)
+                    return 0;
+            }
+            return null;
+        }
+    }
+}
