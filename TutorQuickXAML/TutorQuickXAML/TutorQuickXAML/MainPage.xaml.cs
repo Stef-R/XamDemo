9aab38af21da022d58cfc62820850ee03d506110
Stef-R <username@user.noreply.github.com>
2016-11-21 13:47:15 +0100

    OnTranslate lagt på og virker

--- TutorQuickXAML/TutorQuickXAML/TutorQuickXAML/MainPage.xaml.cs
+++ TutorQuickXAML/TutorQuickXAML/TutorQuickXAML/MainPage.xaml.cs
@@ -9,9 +9,24 @@ namespace TutorQuickXAML
 {
     public partial class MainPage : ContentPage
     {
+        string translatedNumber;
         public MainPage()
         {
             InitializeComponent();
         }
+        void OnTranslate(object sender, EventArgs e)
+        {
+            translatedNumber = "234";// Core.P;
+            if (!string.IsNullOrWhiteSpace(translatedNumber))
+            {
+                callButtom.IsEnabled = true;
+                callButtom.Text = "Call" + translatedNumber;
+            } else {
+                callButtom.IsEnabled = false;
+                callButtom.Text = "Call";
+            }
+        }
+
+
     }
 }
