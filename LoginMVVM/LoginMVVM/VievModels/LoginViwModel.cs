66a4455e980f1efeefd0f9f68ab5ffc7b6ba58b9
Stef-R <username@user.noreply.github.com>
2016-11-28 14:33:08 +0100

    add view and pages

---
+++ LoginMVVM/LoginMVVM/VievModels/LoginViwModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginMVVM.VievModels 
+{
+    class LoginViwModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // simple func
+
+        public virtual void OnProertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var ev = PropertyChanged;
+            if (ev != null)
+            {
+                ev(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
